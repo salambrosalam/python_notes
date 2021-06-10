30ad843fc4846a825403c16338afaca8c383e9ac
salambrosalam <romashkin1273@gmail.com>
2021-06-10 16:02:32 +0200

    some progress

--- Strings.py
+++ Strings.py
@@ -1,3 +1,16 @@
 ### Strings
 
 string = "String"
+name = "serg Bro"
+
+print(name.title())#All words will start from capital letters
+print(name.upper())#All symbols will in UPPER CASE
+print(name.lower())#All symbols will in lower_case
+print("Here some languages: \n\tPython\n\tJava\n\tC++")#\n - new string, \t - tabulation
+a = ".....name...."
+
+print(a.rstrip())#will delete spaces from right
+print(a.lstrip())#will delete space from left
+print(a.strip())#will delete space from from left and right
+print(a.strip("."))#wiil delete all "."
+
---
+++ numbers.py
@@ -0,0 +1,9 @@
+#numbers lesson
+num1 = 22
+num2 = 33
+
+name = "serg"
+
+print (num1 + num2)
+print(name*20)
+
